1b52e24f4abbfc41b5b33c275f8749b3dd5dbdde
Bhardwaj1 <bhardwajgaurav204@gmail.com>
2026-08-19 10:41:50 +0530

    changes in api endpoint

--- frontendResumeViewer/src/api/resume.api.ts
+++ frontendResumeViewer/src/api/resume.api.ts
@@ -1,6 +1,6 @@
 import type { ReviewApiResponse, ReviewData } from "../types/resume.types";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
 export async function reviewResume(resumeFile: File): Promise<ReviewData> {
   const formData = new FormData();
