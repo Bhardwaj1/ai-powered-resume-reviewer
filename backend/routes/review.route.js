5570854ece45f336c557d0f85daaaf05ce482f21
Bhardwaj1 <bhardwajgaurav204@gmail.com>
2026-08-22 01:34:50 +0530

    changes in api endpoint

--- backend/routes/review.route.js
+++ backend/routes/review.route.js
@@ -4,8 +4,22 @@ const multer = require("multer");
 
 const router = express.Router();
 
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "application/pdf") {
+    cb(null, true);
+  } else {
+    cb(new Error("Only Pdf file are allowed"), false);
+  }
+};
+
 const upload = multer({
-  storage: multer.memoryStorage(),
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
 });
 
 router.post("/", upload.single("resume"), reviewResume);
